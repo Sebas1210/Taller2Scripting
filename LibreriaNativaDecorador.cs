ce9090fe8b6a1a4645ac4d4c4ea8b3d79e88866e
Rafael Castaño <114263250+Rafacmzz@users.noreply.github.com>
2024-09-25 11:47:17 -0500

    Add files via upload

---
+++ LibreriaNativaDecorador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "example.txt";
+
+        
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+        {
+            byte[] data = Encoding.UTF8.GetBytes("Este es un ejemplo de patrón decorador en C#.");
+            fileStream.Write(data, 0, data.Length);
+        }
+
+        
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        using (BufferedStream bufferedStream = new BufferedStream(fileStream))
+        using (StreamReader reader = new StreamReader(bufferedStream))
+        {
+            
+            string content = reader.ReadToEnd();
+            Console.WriteLine("Contenido del archivo: ");
+            Console.WriteLine(content);
+        }
+
+        
+        File.Delete(filePath);
+    }
+}
